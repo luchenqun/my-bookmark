f1fe08948c8b753d5d4f9f328afb7c119d410d88
luchenqun <luchenqun@qq.com>
2016-11-01 23:22:05 +0800

    完成登陆界面

--- public/scripts/controllers/login-controller.js
+++ public/scripts/controllers/login-controller.js
@@ -1,29 +1,47 @@
 app.controller('loginCtr', ['$scope', '$filter', '$state', 'bookmarkService', 'pubSubService', function($scope, $filter, $state, bookmarkService, pubSubService) {
     console.log("Hello loginCtr...");
-    login({
-        userName: 'luchenqun',
-        pwd: '123456',
-    });
 
-    function login(params) {
-        bookmarkService.login(params).then(
-            function(data) {
-                console.log(data);
-                if (data.logined) {
-                    pubSubService.publish('loginCtr.login', {
-                        'login': data.logined,
-                    });
-                    $state.go('bookmarks', {
-                        showStyle: 'navigate',
-                    })
-                } else {
-                    console.log('login failed......................')
-                }
-            },
-            function(errorMsg) {
-                console.log(errorMsg);
-            }
-        );
+    $scope.userName = "";
+    $scope.pwd = "";
+    $scope.showErr = false;
+    $scope.errInfo = '';
+
+    $scope.login = function() {
+        var atuoLogin = $('.ui.checkbox.js-auto-login').checkbox('is checked');
+        if (!$scope.userName || !$scope.pwd) {
+            $scope.showErr = true;
+            $scope.errInfo = '用户明或者密码不能为空！';
+        } else {
+            $scope.showErr = false;
+            $scope.errInfo = '';
+            console.log($scope.userName, $scope.pwd, atuoLogin);
+        }
     }
+
+    // login({
+    //     userName: 'luchenqun',
+    //     pwd: '123456',
+    // });
+    //
+    // function login(params) {
+    //     bookmarkService.login(params).then(
+    //         function(data) {
+    //             console.log(data);
+    //             if (data.logined) {
+    //                 pubSubService.publish('loginCtr.login', {
+    //                     'login': data.logined,
+    //                 });
+    //                 $state.go('bookmarks', {
+    //                     showStyle: 'navigate',
+    //                 })
+    //             } else {
+    //                 console.log('login failed......................')
+    //             }
+    //         },
+    //         function(errorMsg) {
+    //             console.log(errorMsg);
+    //         }
+    //     );
+    // }
 
 }]);

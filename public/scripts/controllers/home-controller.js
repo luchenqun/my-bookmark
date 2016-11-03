dbc7ba38166aade8c530fb8cf87ba54c7c776f05
luchenqun <luchenqun@qq.com>
2016-11-03 23:00:11 +0800

    提交更改

--- public/scripts/controllers/home-controller.js
+++ public/scripts/controllers/home-controller.js
@@ -1,21 +1,46 @@
 app.controller('homeCtr', ['$scope', '$stateParams', '$filter', '$state', '$window', 'bookmarkService', 'pubSubService', function($scope, $stateParams, $filter, $state, $window, bookmarkService, pubSubService) {
     console.log('Hello homeCtr......');
-    bookmarkService.autoLogin().then(
-        function(data) {
-            console.log(data);
-            if (data.logined) {
-                pubSubService.publish('loginCtr.login', {
-                    'login': data.logined,
-                });
-                $state.go('bookmarks', {
-                    showStyle: 'navigate',
-                })
-            } else {
-                console.log('login failed......................')
+    var debug = true;
+    if (debug) {
+        bookmarkService.login({
+            username: 'luchenqun',
+            password: 'fendoubuxi',
+            autoLogin: true,
+        }).then(
+            function(data) {
+                console.log(data);
+                if (data.logined) {
+                    pubSubService.publish('loginCtr.login', {
+                        'login': data.logined,
+                    });
+                    $state.go('bookmarks', {
+                        showStyle: 'navigate',
+                    })
+                } else {
+                    console.log('login failed......................')
+                }
+            },
+            function(errorMsg) {
+                console.log(errorMsg);
             }
-        },
-        function(errorMsg) {
-            console.log(errorMsg);
-        }
-    );
+        );
+    } else {
+        bookmarkService.autoLogin().then(
+            function(data) {
+                if (data.logined || debug) {
+                    pubSubService.publish('loginCtr.login', {
+                        'login': debug || data.logined,
+                    });
+                    $state.go('bookmarks', {
+                        showStyle: 'navigate',
+                    })
+                } else {
+                    console.log('login failed......................')
+                }
+            },
+            function(errorMsg) {
+                console.log(errorMsg);
+            }
+        );
+    }
 }]);

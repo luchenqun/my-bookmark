8962c1cb81bc01b32c7f475306dc39ce16897d13
luchenqun <luchenqun@qq.com>
2016-11-10 22:45:12 +0800

    开始添加编辑功能

--- public/scripts/controllers/bookmarks-controller.js
+++ public/scripts/controllers/bookmarks-controller.js
@@ -4,7 +4,7 @@ app.controller('bookmarksCtr', ['$scope', '$state', '$stateParams', '$filter', '
     $scope.showSearch = false; // 书签数据
     $scope.hoverItem = false;
     $scope.showStyle = 'navigate'; // 显示风格'navigate', 'card', 'table'
-    $scope.b = [];
+    $scope.edit = false;
     semanticInit();
 
     var params = {

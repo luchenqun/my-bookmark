671b01efd00f5a8237525a55238f9567a4bac38a
luchenqun <luchenqun@qq.com>
2017-02-22 13:50:19 +0800

    更新一下数据库语句

--- database/db.js
+++ database/db.js
@@ -12,19 +12,19 @@ var client = {}
 function handleDisconnect() {
     client = mysql.createConnection(dbConfig);
 
-    client.connect(function(err) { // The server is either down
-        if (err) { // or restarting (takes a while sometimes).
+    client.connect(function(err) {
+        if (err) {
             console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        } // to avoid a hot loop, and to allow our node script to
-    }); // process asynchronous requests in the meantime.
-    // If you're also serving http, display a 503 error.
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+
     client.on('error', function(err) {
         console.log('db error', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect(); // lost due to either server restart, or a
-        } else { // connnection idle timeout (the wait_timeout
-            throw err; // server variable configures this)
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            handleDisconnect();
+        } else {
+            throw err;
         }
     });
 }
@@ -415,7 +415,7 @@ db.getBookmarksTable = function(params) {
     if (user_id) {
         sql += " AND `user_id` = '" + user_id + "'";
         if (params.showStyle == 'card') {
-            sql += " ORDER BY created_at DESC";
+            sql += " ORDER BY bookmarks.created_at DESC";
         } else {
             sql += " ORDER BY click_count DESC";
         }

cc7994fe5009dc7cae85c8983c79e54bcdd9576d
luchenqun <luchenqun@qq.com>
2017-03-02 11:49:46 +0800

    更新一下动画

--- public/scripts/controllers/home-controller.js
+++ public/scripts/controllers/home-controller.js
@@ -22,7 +22,7 @@ app.controller('homeCtr', ['$scope', '$stateParams', '$filter', '$state', '$wind
         .catch((err) => {
             console.log('autoLogin err', err)
         });
-
+        $('.js-segment-home').transition('hide');
         function transition() {
             var data = ['scale', 'fade', 'fade up', 'fade down', 'fade left', 'fade right', 'horizontal flip',
                 'vertical flip', 'drop', 'fly left', 'fly right', 'fly up', 'fly down', 'swing left', 'swing right', 'swing up',

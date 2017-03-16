6d10974bebc83d6975efbc74f42ea3c8091d2564
luchenqun <luchenqun@qq.com>
2017-03-16 22:01:13 +0800

    增加热门书签收藏的表

--- schema.sql
+++ schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE `bookmarks` (
   `user_id` int(11) DEFAULT NULL,           -- 用户id
   `title` varchar(255) DEFAULT NULL,        -- 标题
   `description` varchar(4096) DEFAULT NULL, -- 描述
-  `url` text,                               -- 链接
+  `url` varchar(1024) DEFAULT NULL,         -- 链接
   `public` tinyint(4) DEFAULT '1',          -- 是否公开 1 公开，0 不公开
   `click_count` smallint DEFAULT 1,         -- 总共点击次数
   `created_at` datetime DEFAULT now(),      -- 创建时间
@@ -61,4 +61,22 @@ CREATE TABLE `advices` (
   `state` tinyint(4) DEFAULT '0',           -- 处理结果
   PRIMARY KEY (`id`),
   KEY `userIdIdx` (`user_id`)
+);
+
+-- 书签表
+drop table if exists hot_bookmarks;
+CREATE TABLE `hot_bookmarks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,     -- id(articleId)
+  `title` varchar(255) DEFAULT NULL,        -- 标题(title)
+  `description` varchar(4096) DEFAULT NULL, -- 描述(自己添加)
+  `url` varchar(1024) DEFAULT NULL,         -- 链接(url)
+  `click_count` smallint DEFAULT 1,         -- 总共点击次数(favCount)
+  `created_by` varchar(64) DEFAULT NULL,    -- 创建者(sourceName)
+  `created_at` bigint DEFAULT 0,            -- 创建时间(updatetime)
+  `last_click` bigint DEFAULT 0,            -- 最后一次点击时间(createtime)
+  `snap_url` varchar(2048) DEFAULT NULL,    -- 截图链接(imageList[0])
+  `favicon_url` varchar(2048) DEFAULT NULL, -- icon链接(sourceLogo)
+  `status` tinyint(4) DEFAULT '0',          -- 状态
+  PRIMARY KEY (`id`),
+  KEY `urlIdx` (`url`)
 );

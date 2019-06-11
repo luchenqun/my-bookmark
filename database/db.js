83ab8f7648cef184b1b1aabe72b29dad350ec13f
luchenqun <luchenqun@qq.com>
2019-06-11 17:22:28 +0800

    去掉固定的配置

--- database/db.js
+++ database/db.js
@@ -6,16 +6,6 @@ try {
     dbConfig = require('../config.default.js').dbConfig;
 }
 
-var dbConfig = {
-    host: '127.0.0.1',
-    user: 'test', // mysql的账号
-    password: '123456', // mysql 的密码
-    database: 'mybookmarks',
-    multipleStatements: true,
-    useConnectionPooling: true,
-    port: 3306
-};
-
 var client = mysql.createConnection(dbConfig);
 
 Date.prototype.format = function(fmt) { //author: meizz

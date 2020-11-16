ef42a5ada7bbf3964380603d98a73baf1dee5b09
B05BEE13.卢郴群 <lucq@vanke.com>
2020-11-16 13:11:49 +0800

    add test user

--- src/bootstrap/worker.js
+++ src/bootstrap/worker.js
@@ -1,8 +1,8 @@
 // invoked in worker
 think.beforeStartServer(async () => {
   const username = 'test';
-  let user = await this.model("users").where({ username }).find();
+  let user = await think.model("users").where({ username }).find();
   if (think.isEmpty(user)) {
-    await this.model("users").add({ username, password: 'e10adc3949ba59abbe56e057f20f883e', email: 'ilovejiajia@qq.com' });
+    await think.model("users").add({ username, password: 'e10adc3949ba59abbe56e057f20f883e', email: 'ilovejiajia@qq.com' });
   }
 })

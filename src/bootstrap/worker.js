c0e3af63c1ce0ad4a8a9628da4e83331d3195d78
B05BEE13.卢郴群 <lucq@vanke.com>
2020-11-23 13:53:49 +0800

    项目自启动服务

--- src/bootstrap/worker.js
+++ src/bootstrap/worker.js
@@ -1,5 +1,11 @@
 // invoked in worker
+const { execSync } = require('child_process');
 think.beforeStartServer(async () => {
+  try {
+    execSync('service mysql start', { stdio: ['inherit', 'inherit', 'inherit'] });
+  } catch (error) {
+    console.log("beforeStartServer", error);
+  }
   const username = 'test';
   let user = await think.model("users").where({ username }).find();
   if (think.isEmpty(user)) {
